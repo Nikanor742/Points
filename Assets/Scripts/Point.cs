7b1ed77e494fac42952e57addd4094f036b174cd
Nikanor <nik14kg@gmail.com>
2021-09-07 16:27:25 +0700

    Initial commit

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void MouseDown()
+    {
+        Debug.Log("Hello");
+    }
+}
